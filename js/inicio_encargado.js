987554d9399f8b9fbb81c1dbcd95724e00129c5e
Florencia Miranda <flo.mirandai@gmail.com>
2019-04-02 23:33:13 +0200

    Se agregan funciones a los botones de encargado

--- js/inicio_encargado.js
+++ js/inicio_encargado.js
@@ -1,3 +1,15 @@
 function irNuevoPedido() {
   $("#slider").load("../html/nuevo_pedido.html");
 }
+
+function irControlDeStock() {
+  $("#slider").load("../html/control_stock.html");
+}
+
+function irVerPedido() {
+  $("#slider").load("../html/ver_pedidos.html");
+}
+
+function irRealizarVenta() {
+  $("#slider").load("../html/hacer_venta.html");
+}
