ae718dce7b8c716942e7e52c24c308fbaffb00e1
Florencia Miranda <flo.mirandai@gmail.com>
2019-04-09 13:35:00 +0200

    prueba sin localstorage

--- js/login.js
+++ js/login.js
@@ -1,29 +1,38 @@
-
 function ingresar() {
-  var usuarios = JSON.parse(window.localStorage.getItem("datos")).usuarios;
-  var user = $("#user").val();
-  var pass = $("#password").val();
+  // var usuarios = JSON.parse(window.localStorage.getItem("datos")).usuarios;
+  // var user = $("#user").val();
+  // var pass = $("#password").val();
 
-  var usuario = usuarios.find(x => x.nombre == user);
-  
-  if (usuario === undefined){
-    alert("Usuario no existe");
-  }
-  else{
-    if (usuario.clave == pass){
-      $('#slider').load("./html/inicio_encargado.html");
-      $("#title-header").hide();
-      $("#menu-nav").show();
-      $("#logout").show();
-    }
-    else{
-      $("#wrong-pass").hide();
-      $("#wrong-pass").show("");
-    }
-  }
+  // var usuario = usuarios.find(x => x.nombre == user);
+
+  // if (usuario === undefined){
+  //   alert("Usuario no existe");
+  // }
+  // else{
+  //   if (usuario.clave == pass){
+  //     $('#slider').load("./html/inicio_encargado.html");
+  //     $("#title-header").hide();
+  //     $("#menu-nav").show();
+  //     $("#logout").show();
+  //   }
+  //   else{
+  //     $("#wrong-pass").hide();
+  //     $("#wrong-pass").show("");
+  //   }
+  // }
+  console.log("se cargo bien!");
+  $("#slider").load("./html/inicio_encargado.html");
 }
 
-function cargaNuevoPedido(){ $("#slider").load("./html/nuevo_pedido.html");}
-function cargaStock(){ $("#slider").load("./html/control_stock.html");}
-function cargaPedidos(){ $("#slider").load("./html/ver_pedidos.html");}
-function cargaVentas(){ $("#slider").load("./html/hacer_venta.html");}
+function cargaNuevoPedido() {
+  $("#slider").load("./html/nuevo_pedido.html");
+}
+function cargaStock() {
+  $("#slider").load("./html/control_stock.html");
+}
+function cargaPedidos() {
+  $("#slider").load("./html/ver_pedidos.html");
+}
+function cargaVentas() {
+  $("#slider").load("./html/hacer_venta.html");
+}
