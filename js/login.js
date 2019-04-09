7980c68bcf3975c0931e081e23646975fa5758af
Florencia Miranda <flo.mirandai@gmail.com>
2019-04-09 13:37:05 +0200

    se deshace el cambio del localstorage

--- js/login.js
+++ js/login.js
@@ -1,27 +1,23 @@
 function ingresar() {
-  // var usuarios = JSON.parse(window.localStorage.getItem("datos")).usuarios;
-  // var user = $("#user").val();
-  // var pass = $("#password").val();
+  var usuarios = JSON.parse(window.localStorage.getItem("datos")).usuarios;
+  var user = $("#user").val();
+  var pass = $("#password").val();
 
-  // var usuario = usuarios.find(x => x.nombre == user);
+  var usuario = usuarios.find(x => x.nombre == user);
 
-  // if (usuario === undefined){
-  //   alert("Usuario no existe");
-  // }
-  // else{
-  //   if (usuario.clave == pass){
-  //     $('#slider').load("./html/inicio_encargado.html");
-  //     $("#title-header").hide();
-  //     $("#menu-nav").show();
-  //     $("#logout").show();
-  //   }
-  //   else{
-  //     $("#wrong-pass").hide();
-  //     $("#wrong-pass").show("");
-  //   }
-  // }
-  console.log("se cargo bien!");
-  $("#slider").load("./html/inicio_encargado.html");
+  if (usuario === undefined) {
+    alert("Usuario no existe");
+  } else {
+    if (usuario.clave == pass) {
+      $("#slider").load("./html/inicio_encargado.html");
+      $("#title-header").hide();
+      $("#menu-nav").show();
+      $("#logout").show();
+    } else {
+      $("#wrong-pass").hide();
+      $("#wrong-pass").show("");
+    }
+  }
 }
 
 function cargaNuevoPedido() {
