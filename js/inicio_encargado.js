10891f3a948dcaa2aadf4db6937643a2a8da583b
ManuGS2 <manu.skate17@gmail.com>
2019-04-09 19:38:39 -0500

    header terminado

--- js/inicio_encargado.js
+++ js/inicio_encargado.js
@@ -1,19 +1,3 @@
-function irNuevoPedido() {
-  $("#slider").load("../html/nuevo_pedido.html");
-}
-
-function irControlDeStock() {
-  $("#slider").load("../html/control_stock.html");
-}
-
-function irVerPedido() {
-  $("#slider").load("../html/pedidos_trabajador.html");
-}
-
-function irRealizarVenta() {
-  $("#slider").load("../html/hacer_venta.html");
-}
-
 function botondiv(elemento) {
   if (elemento.childNodes[1].checked) {
     elemento.childNodes[3].innerText = "Disponible";
--- js/login.js
+++ js/login.js
@@ -11,10 +11,12 @@ function ingresar() {
   }
   else{
     if (usuario.clave == pass){
-      $('#slider').load("./html/inicio_encargado.html");
+      $("#breadcrumb").show();
+      cargaInicio();
+      $("#image-header").hide();
       $("#title-header").hide();
       $("#menu-nav").show();
-      $("#logout").show();
+      $
     }
     else{
       $("#wrong-pass").hide();
@@ -23,7 +25,28 @@ function ingresar() {
   }
 }
 
-function cargaNuevoPedido(){ $("#slider").load("./html/nuevo_pedido.html");}
-function cargaStock(){ $("#slider").load("./html/control_stock.html");}
-function cargaPedidos(){ $("#slider").load("./html/ver_pedidos.html");}
-function cargaVentas(){ $("#slider").load("./html/hacer_venta.html");}
+function cargaNuevoPedido(){
+  $("#slider").load("./html/nuevo_pedido.html");
+  $(".breadcrumb-item").remove();
+  $("#breadcrumb").append("<li class='breadcrumb-item'><a href='#' onclick='cargaInicio();'>Inicio</a></li><li class='breadcrumb-item' aria-current='page'>Nuevo Pedido</li>");
+}
+function cargaStock(){
+  $("#slider").load("./html/control_stock.html");
+  $(".breadcrumb-item").remove();
+  $("#breadcrumb").append("<li class='breadcrumb-item'><a href='#' onclick='cargaInicio();'>Inicio</a></li><li class='breadcrumb-item' aria-current='page'>Stock</li>");
+}
+function cargaPedidos(){ 
+  $("#slider").load("./html/ver_pedidos.html");
+  $(".breadcrumb-item").remove();
+  $("#breadcrumb").append("<li class='breadcrumb-item'><a href='#' onclick='cargaInicio();'>Inicio</a></li><li class='breadcrumb-item' aria-current='page'>Pedidos</li>");
+}
+function cargaVentas(){
+  $("#slider").load("./html/hacer_venta.html");
+  $(".breadcrumb-item").remove();
+  $("#breadcrumb").append("<li class='breadcrumb-item'><a href='#' onclick='cargaInicio();'>Inicio</a></li><li class='breadcrumb-item' aria-current='page'>Ventas</li>");
+}
+function cargaInicio(){
+  $("#slider").load("./html/inicio_encargado.html");
+  $(".breadcrumb-item").remove();
+  $("#breadcrumb").append("<li class='breadcrumb-item'>Inicio</li>");
+}
