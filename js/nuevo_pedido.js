8c9bdf768e109cf21f0b6b7955cb9c50f5c84b29
Florencia Miranda <flo.mirandai@gmail.com>
2019-04-23 23:24:55 +0200

    funcionalidad nuevo pedido

--- js/nuevo_pedido.js
+++ js/nuevo_pedido.js
@@ -33,7 +33,7 @@ function buscarId(idCliente) {
 
 function crearCliente() {
   $("#modalNuevoCliente").modal("hide");
-  $(".alert").css("display", "flex");
+  $("#alertaCliente").css("display", "flex");
   $("#idc").val($("#idClienteNuevo").val());
   var nombre = $("#nombreClienteNuevo").val();
   mostrarCliente(nombre);
@@ -42,15 +42,24 @@ function crearCliente() {
 function cargarSelects() {
   $(".option-selected").html(" -- Seleccionar --");
 
-  $(".select").on("click", function () {
-    if ($(this).children(".option").css("display") == "none") {
-      $(this).children(".option").css("display", "block");
-      $(this).children(".hr").css("display", "block");
+  $(".select").on("click", function() {
+    if (
+      $(this)
+        .children(".option")
+        .css("display") == "none"
+    ) {
+      $(this)
+        .children(".option")
+        .css("display", "block");
+      $(this)
+        .children(".hr")
+        .css("display", "block");
       // $(".option-selected").css("padding-bottom","10px");
-      $(this).children(".arrow").css("color", "blue");
+      $(this)
+        .children(".arrow")
+        .css("color", "blue");
       $(this).css("z-index", "999");
-    }
-    else {
+    } else {
       $(".option").css("display", "none");
       $(".hr").css("display", "none");
       $(".option-selected").css("padding-bottom", "5px");
@@ -59,12 +68,45 @@ function cargarSelects() {
     }
   });
 
-  $(".option").on("click", function () {
+  $(".option").on("click", function() {
     if ($(".option").attr("data") != "undefined") {
-      $(this).parent().children(".option-selected").html($(this).html());
+      $(this)
+        .parent()
+        .children(".option-selected")
+        .html($(this).html());
       $(".option").removeAttr("data");
       $(this).attr("data", "selected");
-      $(this).parent().children("select").attr("value", $(this).attr("value"));
+      $(this)
+        .parent()
+        .children("select")
+        .attr("value", $(this).attr("value"));
     }
   });
+}
+
+function agregarPedido1() {
+  var tipo_prenda = $("#tipo_prenda")[0].attributes.value.value;
+  var tipo_tela = $("#tipo_tela")[0].attributes.value.value;
+  var modista = $("#checkModista")[0].checked;
+  var tipo_servicio = $("#tipo_servicio")[0].childNodes;
+
+  var id_trabajador = $("#idt").val();
+  var id_cliente = $("#idc").val();
+  var observaciones = $("textarea#obs").val();
+  console.log("", tipo_prenda);
+
+  console.log("tipotela", tipo_tela);
+
+  if (
+    tipo_prenda != 0 &&
+    tipo_tela != 0 &&
+    id_cliente != "" &&
+    id_trabajador != ""
+  ) {
+    $("#slider").load("./html/inicio_encargado.html", () => {
+      $("#alertaPedido").css("display", "flex");
+    });
+  } else {
+    $("#mensajeError").css("display", "flex");
+  }
 }
